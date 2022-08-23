21f867e86b687c2cad2c899517a27b5b2dbba422
ksongbcit <ksong34@my.bcit.ca>
2022-08-22 23:13:03 -0700

    user reg/login/auth/routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,16 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
+
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 
@@ -16,9 +22,13 @@ const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
-    } else {
-        res.status(200).json({ message: 'Set goal' })
     }
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 
 })
 
@@ -27,7 +37,17 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route   POST /api/goals/id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedGoal)
 })
 
 
@@ -35,7 +55,17 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route   GET /api/goals/id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
