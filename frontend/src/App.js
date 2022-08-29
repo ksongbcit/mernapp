0deabf9b11f9b31ab38fe25b0e3f60d643304dfc
ksongbcit <ksong34@my.bcit.ca>
2022-08-28 18:36:50 -0700

    added frontend ui

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          {/** only allows "Route"'s inside "Routes" */}
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
